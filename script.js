ed7a690a549664d11956195d7cbc5b331855c73e
Tyler Thietje <tylerthietje@gmail.com>
2022-07-13 18:25:55 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Javascript Fundamentals Code Along
+
+console.log(23 + 97);
+console.log(1 + 5 + 8 + 9 + 3 + 7);
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+9 * a;
+let b = 7 * a;
+console.log(b);
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
