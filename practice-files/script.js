bdbb9c60b577e39622848d42b0a89bc3ad9a4937
Tyler Thietje <tylerthietje@gmail.com>
2022-07-21 14:19:30 -0500

    updating files to include completed exercises and lessons.

---
+++ practice-files/script.js
@@ -0,0 +1,11 @@
+// const myObj = new Object();
+
+// console.log(myObj.constructor);
+
+// function Account() { };
+
+// const userAccount = new Account(); //prototype attribute is Account.prototype
+
+// var myArray = new Array();
+// console.log(myArray.constructor);
+
